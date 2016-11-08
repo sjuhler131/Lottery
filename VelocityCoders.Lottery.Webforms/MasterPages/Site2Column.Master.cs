5dc711c8fbd6fb47216f15694ead46558375ec38
VC-017 <VC-017@DESKTOP-GMSPFPN>
2016-11-08 13:33:58 -0600

    Created a Master sheet and did nothing with it yet.

---
+++ VelocityCoders.Lottery.Webforms/MasterPages/Site2Column.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VelocityCoders.Lottery.Webforms.MasterPages
+{
+    public partial class Site2Column : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
